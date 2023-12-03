0cf34b8a6e2a77a2a187ecd65863f95e811b3ec8
Juantrevi <juantrevi70@gmail.com>
2023-12-03 14:43:46 +0000

    -- include() on returning --

--- Infrastructure/Data/ProductRepository.cs
+++ Infrastructure/Data/ProductRepository.cs
@@ -21,15 +21,26 @@ public class ProductRepository : IProductRepository
     
     public async Task<Product> GetProductByIdAsync(int id)
     {
-        return await _context.Products.FindAsync(id);
+        //include()
+        //This method is used to include related entities in the query results.
+        return await _context.Products
+            .Include(p => p.ProductType)
+            .Include(p => p.ProductBrand)
+            .FirstOrDefaultAsync(p => p.Id == id);
     }
 
     
     public async Task<IReadOnlyList<Product>> GetProductsAsync()
     {
-        return await _context.Products.ToListAsync();
+        //include()
+        //This method is used to include related entities in the query results.
+        return await _context.Products
+            .Include(p => p.ProductType)
+            .Include(p => p.ProductBrand)
+            .ToListAsync();
     }
 
+    
     public async Task<IReadOnlyList<ProductBrand>> GetProductBrandsAsync()
     {
         return await _context.ProductBrands.ToListAsync();
