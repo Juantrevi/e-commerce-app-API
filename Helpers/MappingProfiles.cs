8ae6a89adcb7f3698bf2464fb25e88276426cfc8
Juantrevi <juantrevi70@gmail.com>
2023-12-06 22:18:14 +0000

    -- Resolver to map pictureUrl --

--- Helpers/MappingProfiles.cs
+++ Helpers/MappingProfiles.cs
@@ -16,7 +16,8 @@ public class MappingProfiles : Profile
         //We need to tell AutoMapper how to map the ProductType and ProductBrand
         CreateMap<Product, ProductToReturnDto>()
             .ForMember(d => d.ProductBrand, o => o.MapFrom(s => s.ProductBrand.Name))
-            .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name));
+            .ForMember(d => d.ProductType, o => o.MapFrom(s => s.ProductType.Name))
+            .ForMember(d => d.PictureUrl, o => o.MapFrom<ProductUrlResolver>());
     }
     
 }
---
+++ Helpers/ProductUrlResolver.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using Core.Entities;
+using e_commerce_app.Dtos;
+
+namespace e_commerce_app.Helpers;
+//Class
+//This class is used to resolve the url of the product image
+//and add the localhost url we placed in the appsettings.json file
+public class ProductUrlResolver : IValueResolver<Product, ProductToReturnDto, string>
+{
+    private readonly IConfiguration _config;
+
+    public ProductUrlResolver(IConfiguration config)
+    {
+        _config = config;
+    }
+    
+    //After
+    //We need to tell AutoMapper (MappingProfile) how to map.
+    public string Resolve(Product source, ProductToReturnDto destination, string destMember, ResolutionContext context)
+    {
+        if(!string.IsNullOrEmpty(source.PictureUrl))
+        {
+            return _config["ApiUrl"] + source.PictureUrl;
+        }
+
+        return null;
+
+    }
+}
