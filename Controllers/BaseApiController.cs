535d78954473fcb89a2784a5de02849021be0b70
Juantrevi <juantrevi70@gmail.com>
2023-12-08 22:38:20 +0000

    -- Documenting --

--- Controllers/BaseApiController.cs
+++ Controllers/BaseApiController.cs
@@ -2,6 +2,10 @@
 
 namespace e_commerce_app.Controllers;
 
+/*
+ * Class BaseApiController
+ * This class is used to extend the ControllerBase class
+ */
 [ApiController]
 [Route("api/[controller]")]
 public class BaseApiController : ControllerBase
