4c3020f20058df0b841b881c3d2f59f080ba7b1f
Juantrevi <juantrevi70@gmail.com>
2023-12-08 23:55:27 +0000

    -- Documentation --

--- Errors/ApiResponse.cs
+++ Errors/ApiResponse.cs
@@ -2,15 +2,20 @@
 
 public class ApiResponse
 {
-    //Class
-    //This class will be used to return a response to the client
-    //that is flattened and has a status code and a message
+    /*
+    Class()
+      This class will be used to return a response to the client
+      that is flattened and has a status code and a message
+    */
+
     public ApiResponse(int statusCode, string message = null)
     {
-        StatusCode = statusCode;
-        //The ??
-        //operator is called the null-coalescing operator
-        //It returns the left-hand operand if the operand is not null; otherwise it returns the right hand operand
+        StatusCode = statusCode; 
+    /*
+    The ??
+    operator is called the null-coalescing operator
+    It returns the left-hand operand if the operand is not null; otherwise it returns the right hand operand
+    */
         Message = message ?? GetDefaultMessageForStatusCode(statusCode);
     }
     public int StatusCode { get; set; }
