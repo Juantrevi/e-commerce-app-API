347c8b31341b86cec6e7ade8c80cec286e9b1607
Juantrevi <juantrevi70@gmail.com>
2023-12-08 23:52:40 +0000

    -- Documentation --

--- Errors/ApiException.cs
+++ Errors/ApiException.cs
@@ -1,16 +1,23 @@
 ﻿namespace e_commerce_app.Errors;
 
-//Class
-//Made to return a response with more information than
-//the message and status code, here we add where the error
-//occurred
+/*
+ *Class()
+    Made to return a response with more information than
+    the message and status code, here we add where the error
+    occurred
+*/
 
-//Need to create Middleware to use this class
-//and handle the exceptions (Exceptions/ExceptionMiddleware.cs
+/*
+  Create Middleware to use this class
+  and handle the exceptions (Exceptions/ExceptionMiddleware.cs
+ */
+
 public class ApiException : ApiResponse
 {
-    //Add
-    //string details to the constructor
+    /*
+     Add
+     string details to the constructor
+     */
     public ApiException(int statusCode, string message = null, string details = null) : base(statusCode, message)
     {
         Details = details;
