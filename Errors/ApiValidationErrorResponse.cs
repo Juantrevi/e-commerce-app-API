be95c506d9f7687fab13180caf9aa4a244090015
Juantrevi <juantrevi70@gmail.com>
2023-12-08 23:59:10 +0000

    -- Documentation --

--- Errors/ApiValidationErrorResponse.cs
+++ Errors/ApiValidationErrorResponse.cs
@@ -2,10 +2,13 @@
 
 public class ApiValidationErrorResponse : ApiResponse
 {
-    //Class
-    // This is the class that will be returned to the client when there is a validation error
-    // It will contain the status code and the errors flattened out
-    //Need to configure Program.cs to use this class as a service
+    
+    /*
+    Class()
+      This is the class that will be returned to the client when there is a validation error
+      It will contain the status code and the errors flattened out
+      Need to configure Program.cs to use this class as a service
+    */
     public ApiValidationErrorResponse() : base(400)
     {
     }
