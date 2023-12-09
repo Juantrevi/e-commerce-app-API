455513a0eead664906e8bdad21afd403da173ad9
Juantrevi <juantrevi70@gmail.com>
2023-12-09 01:15:10 +0000

    -- Documentation --

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -4,11 +4,12 @@ using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Data;
 
-//Creating Migrations:
-// dotnet ef database drop -p Infrastructure
-// dotnet ef migrations remove -p Infrastructure
-// dotnet ef migrations add InitialCreate -p Infrastructure -o Data/Migrations
-
+/*
+Creating Migrations:
+dotnet ef database drop -p Infrastructure
+dotnet ef migrations remove -p Infrastructure
+dotnet ef migrations add InitialCreate -p Infrastructure -o Data/Migrations
+*/
 public class StoreContext : DbContext
 {
     public StoreContext(DbContextOptions options) : base(options)
@@ -20,8 +21,10 @@ public class StoreContext : DbContext
     public DbSet<ProductType> ProductTypes { get; set; }
 
 
-    //This method is used to configure our entities as we did it in the Config folder 
-    //ProductConfiguration class
+    /*
+    This method is used to configure our entities as we did it in the Config folder 
+    ProductConfiguration class 
+    */
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
